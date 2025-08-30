1ea0566b65df508100666177d4dcf1cdc0e89a8a
Dheeraj Rauniyar <dheerajrauniyar123@gmail.com>
2025-08-30 12:30:20 +0530

    making databse connection and makee singnup api

--- src/app.js
+++ src/app.js
@@ -1,14 +1,36 @@
-const express = require('express');
+const express = require("express");
+const dotenv = require("dotenv");
+const connectDB = require("./config/database");
+const User = require("./models/user");
 
+dotenv.config();
 const app = express();
-app.use('/',(req, res) => {
-    res.send("this is the data");
-})
+app.use(express.json());
 
-app.use('/hello',(req, res) => {
-    res.send("this is the data from my server");
-})
 
-app.listen(3000, () => {
-    console.log("Our server is running successfully on port number 3000 ")
-})
+app.post("/signup", async (req, res) => {
+    try {
+        console.log("👉 Incoming Data:", req.body);
+
+        const user = new User(req.body);
+        await user.save();
+
+        res.status(201).json({
+            message: "User created successfully",
+            data: user,
+        });
+    } catch (error) {
+        console.error("❌ Error: " + error.message);
+        res.status(500).json({
+            message: "Something went wrong",
+            error: error.message,
+        });
+    }
+});
+
+
+connectDB().then(() => {
+    app.listen(process.env.PORT || 5000, () => {
+        console.log(`🚀 Server running on port: ${process.env.PORT || 5000}`);
+    });
+});
